f32051cf373aba19cd25a03aece63310ae75e11f
Raghav Suryanarayanan <raghav.suryanarayanan@bglgroup.co.uk>
2018-12-08 23:41:35 +0000

    initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,81 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hashinput
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({x, y}) ->
+      :egd.filledRectangle(image, x, y, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_, index}) ->
+      x_axis_horizontal = rem(index, 5) * 50
+      x_axis_vertical = div(index, 5) * 50
+
+      top_left = {x_axis_horizontal, x_axis_vertical}
+      bottom_right = {x_axis_horizontal + 50 , x_axis_vertical + 50 }
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+
+  end
+  def filter_odd_squares( %Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code , 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid( %Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+      %Identicon.Image{image | grid: grid}
+  end
+
+
+  def mirror_row(row) do
+    [first, second | _] = row
+
+    row++ [second, first]
+  end
+
+  def pick_color( %Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r,g,b}}
+  end
+  def hashinput(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def hello do
+    :world
+  end
+end
